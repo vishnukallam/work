bfd3fd6a8101c16a58b9b5b48742ee4ef0f81616
Vishnu Kallam <127661438+Vishnudluffy@users.noreply.github.com>
2023-09-17 20:58:13 +0530

    beta.py

--- python/beta.py
+++ python/beta.py
@@ -1,11 +1,23 @@
-from playsound import playsound
-stack =[]
-stack.append(r"E:\ZORO\Vishnudluffy\python\one piece songs.mp3")
-stack.append(r"E:\ZORO\Vishnudluffy\python\Imagine Dragons - Bones (Official Audio).mp3")
-stack.append(r"E:\ZORO\Vishnudluffy\python\Imagine Dragons x JID - Enemy.mp3")
-stack.append(r"E:\ZORO\Vishnudluffy\python\Indila - Dernière Danse.mp3")
-stack.append(r"E:\ZORO\Vishnudluffy\python\Suzume No Tojimari Title Track [Kan Rom Eng] すずめの戸締まり 歌詞.mp3")
-playsound(stack[1])
-# for i in stack[4]:
-#playsound(i)
-# print(stack.append(E:\ZORO\Vishnudluffy\python\Suzume No Tojimari Title Track [Kan Rom Eng] すずめの戸締まり 歌詞.mp3))
+import pygame
+# os.startfile(r"E:\ZORO\Vishnudluffy\python\music")
+# folder_path = "E:\ZORO\Vishnudluffy\python\music"
+# pygame.init()
+# music_files = [file for file in os.listdir(folder_path) if file.endswith((".mp3", ".wav"))]
+# for music_file in music_files:
+#     music_path = os.path.join(folder_path, music_file)
+#     pygame.mixer.music.load(music_path)
+#     pygame.mixer.music.play()
+#     while pygame.mixer.music.get_busy():
+#         pass
+# pygame.quit()
+pygame.init()
+screen = pygame.display.set_mode((800 , 700))
+pygame.display.set_caption("My new game.")
+icon = pygame.image.load("naruto.png")
+pygame.display.set_icon(icon)
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
