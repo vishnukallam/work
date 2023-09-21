f19046f5500132805f2826dcb2ab19e4a029e770
Vishnu Kallam <127661438+Vishnudluffy@users.noreply.github.com>
2023-09-21 21:51:52 +0530

    bet.py

--- python/bet.py
+++ python/bet.py
@@ -1,14 +1,14 @@
-# T =  int(input("enter a number: "))
-# for i in range(T):
-#     H,X,Y =map(int,input().split())
-#     if H%(X+Y) == 0:
-#         print("1")
-#     else:
-#         print("0")
-from pydub import AudioSegment
+# # T =  int(input("enter a number: "))
+# # for i in range(T):
+# #     H,X,Y =map(int,input().split())
+# #     if H%(X+Y) == 0:
+# #         print("1")
+# #     else:
+# #         print("0")
+# from pydub import AudioSegment
 
-# Load an audio file (e.g., in MP3 format)
-audio = AudioSegment.from_mp3("input.mp3")
+# # Load an audio file (e.g., in MP3 format)
+# audio = AudioSegment.from_mp3("input.mp3")
 
-# Convert the audio to WAV format
-audio.export("output.wav", format="wav")
+# # Convert the audio to WAV format
+# audio.export("output.wav", format="wav")
