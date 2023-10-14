d4b0f377b3d0e8c2251a3b1adbe4a448d950d166
Vishnu Kallam <127661438+Vishnudluffy@users.noreply.github.com>
2023-10-14 11:35:40 +0530

    .\arm.cpp

--- C++/arm.cpp
+++ C++/arm.cpp
@@ -1,24 +1,27 @@
-
 #include<iostream>
-#include<math.h>
 using namespace std;
+class a{
+    public:
+        int i = 7;
+        void show(){
+            cout<< i;
+        }
+};
+class b: virtual public a{
+    public :
+        int j;
+};
+class c: virtual public a{
+    public:
+    int k;
+};
+class d : public b, public c{
+    public:
+    int j;
+};
 int main(){
-    int num,org_num,rem,result=0,count=0;
-    cout<<"enter a number:";
-    cin>>num;
-    org_num=num;
-    while (org_num !=0){
-    	count++;
-		org_num = org_num\10;	
-	}
-    while (org_num != 0){
-        rem = org_num % 10;
-        result = result + pow(rem,count);
-        org_num = org_num / 10;
-    }
-    if (result == num)
-        cout << "the given number is armstrong number";
-    else
-        cout<< "the given number is not an armstromg number";
+    d object;
+    object.i = 6;
+    object.show();
     return 0;
 }
