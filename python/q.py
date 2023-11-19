675b00908b0aab92819debf50e1b3748ca3ec4c5
Vishnu Kallam <127661438+Vishnudluffy@users.noreply.github.com>
2023-11-19 14:41:04 +0530

    .\python\

--- python/q.py
+++ python/q.py
@@ -1,26 +1,33 @@
 import tkinter
 from tkinter import *
+
+
 class okquit:
     def __init__(self):
-        self.window=Tk()
-        self.window.tile='OK & QUIT DEMO'
+        self.window = Tk()
+        self.window.tile = 'OK & QUIT DEMO'
         self.window.geometry('400x400')
-        self.text=Entry(self.window,width=15)
+        self.text = Entry(self.window, width=15)
         self.text.pack()
-        self.lbl=Label(self.window,text="",font=('Ariel Bold',16))
+        self.lbl = Label(self.window, text="", font=('Ariel Bold', 16))
         self.lbl.pack()
-        self.btnok=Button(self.window,text="OK",command=self.clickok)
+        self.btnok = Button(self.window, text="OK", command=self.clickok)
         self.btnok.pack()
-        self.btnquit=Button(self.window,text="QUIT",command=self.clickquit)
+        self.btnquit = Button(self.window, text="QUIT", command=self.clickquit)
         self.btnquit.pack()
         self.window.mainloop()
+
     def clickok(self):
-        if len(self.text.get()) == 0: 
-            message="Please Enter Data"
+        if len(self.text.get()) == 0:
+            message = "Please Enter Data"
             self.lbl.conﬁgure(text=message)
         else:
             print(self.text.get())
-            self.text.delete(0,END)
+            self.text.delete(0, END)
+
     def clickquit(self):
         self.window.destroy()
+
+
 okquit()
+print(type(okquit))
--- python/sample.py
+++ python/sample.py
@@ -1,1 +1,14 @@
-print("hello world")
+class student:
+    def __init__(self, age, name) -> int:
+        self.age = age
+        self.name = name
+
+    def show(self):
+        return self.age, self.name
+
+
+clas = student(12, "revi")
+print(clas.show())
+print(type(clas))
+print(id(clas))
+print(id(student))
