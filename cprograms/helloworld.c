2901404cb35017e89e02dc2195401180bc9c4774
Vishnu Kallam <127661438+Vishnudluffy@users.noreply.github.com>
2023-11-19 14:40:06 +0530

    .\cprograms\

--- cprograms/helloworld.c
+++ cprograms/helloworld.c
@@ -1,11 +1,50 @@
-#include<stdio.h>
-    int main(){
-        int n,fact = 1,i;
-        n = 5;
-        for(i=1;i<=n;i++){
-            fact = fact *i;
+#include <stdio.h>
+#include <stdlib.h>
+int check(int *string, int *frames, int pos, int f_size)
+{
+    for (int i = 0; i < f_size; i++)
+    {
+        if (*(string + pos) == *(frames + i))
+        {
+            return 1;
         }
-        printf("%d",fact);
-        return 0;
-
     }
+    return 0;
+}
+int main()
+{
+    int i, j, str_len, f_size, *string = NULL, *frames = NULL, faults = 0;
+    printf("Enter string length:");
+    scanf("%d", &str_len);
+    printf("Enter no.of frames:");
+    scanf("%d", &f_size);
+    string = (int *)malloc(sizeof(int) * str_len);
+    frames = (int *)malloc(sizeof(int) * f_size);
+    printf("Enter reference string:");
+    for (i = 0; i < str_len; i++)
+    {
+        scanf("%d", string + i);
+    }
+    for (i = 0; i < f_size; i++)
+    {
+        *(frames + i) = -1;
+    }
+    j = 0;
+    for (i = 0; i < str_len; i++)
+    {
+        if (!check(string, frames, i, f_size))
+        {
+            faults++;
+            *(frames + j) = *(string + i);
+            j = (j + 1) % f_size;
+        }
+        for (int k = 0; k < f_size; k++)
+        {
+            printf("%d ", *(frames + k));
+        }
+        printf("\n");
+    }
+    printf("The no.of faults are %d\n", faults);
+    free(string);
+    free(frames);
+}
--- cprograms/num.c
+++ cprograms/num.c
@@ -1,11 +1,13 @@
-#include<stdio.h>
-long int main(){
-    int n,i,n1,tot=1;
+#include <stdio.h>
+long int main()
+{
+    int n, i, n1, tot = 1;
     printf("enter numbers : ");
-    scanf("%d%d",&n,&n1);
-    for(i+0;i<=n;i++){
-        tot =tot*10+n*n1;       
+    scanf("%d%d", &n, &n1);
+    for (i + 0; i <= n; i++)
+    {
+        tot = tot * 10 + n * n1;
     }
-    printf("tot %ld",tot);
+    printf("tot %ld", tot);
     return 0;
 }
